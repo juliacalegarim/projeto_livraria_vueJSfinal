91413012006b34a6afc4d73475f71622b6e6df25
juliacalegarim <jujucalegarim@gmail.com>
2026-08-03 23:45:19 -0300

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,22 @@
-// Este arquivo é responsável por configurar as rotas do aplicativo Vue.js usando o Vue Router. Ele define as rotas para as principais páginas da aplicação, como a página inicial (HomeView), a página de listagem de produtos (ProductList) e a página do carrinho de compras (CartPanel). Cada rota é associada a um componente específico que será renderizado quando o usuário navegar para essa rota. O router é criado usando a função createRouter e exportado para ser usado em outras partes do aplicativo, como na configuração principal do Vue.js.
-
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/components/layout/HomeView.vue'
-import ProductList from '@/components/products/ProductList.vue'
-import CartPanel from '@/components/cart/CartPanel.vue'
+
+import HomeView from '../views/HomeView.vue'
+import CarrinhoView from '../views/CarrinhoView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/produtos',
-      name: 'products',
-      component: ProductList,
+      name: 'Home',
+      component: HomeView
     },
     {
       path: '/carrinho',
-      name: 'cart',
-      component: CartPanel,
-    },
-  ],
+      name: 'Carrinho',
+      component: CarrinhoView
+    }
+  ]
 })
 
 export default router
