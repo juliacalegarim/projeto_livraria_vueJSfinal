5b0d182a6725c5d09e79ac7946100d59755ca29d
pietracavalheiro27 <cavalheirojpietra@gmail.com>
2026-08-03 23:43:06 -0300

    main

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
-// Este arquivo é o ponto de entrada principal do aplicativo Vue.js. Ele importa os estilos globais, cria a instância do aplicativo, configura o roteamento e monta o aplicativo na página HTML. O main.js é essencial para iniciar a aplicação e garantir que todos os componentes e rotas estejam corretamente configurados para serem usados em toda a aplicação.
-
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import './style.css'
 
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
